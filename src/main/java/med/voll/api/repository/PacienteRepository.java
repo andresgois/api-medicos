d829fbb076cd3f82203a1e2e7047e4c0aec14607
Francisco Andre De Sousa Gois <francisco.gois@pdcase.com.br>
2022-11-20 10:44:05 -0300

    :sparkles:	feat: repositories

---
+++ src/main/java/med/voll/api/repository/PacienteRepository.java
@@ -0,0 +1,8 @@
+package med.voll.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import med.voll.api.paciente.Paciente;
+
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+}
