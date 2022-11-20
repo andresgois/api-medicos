ba5a16797c9d484216d8fca0b6f4010db1a214ea
Francisco Andre De Sousa Gois <francisco.gois@pdcase.com.br>
2022-11-20 10:43:48 -0300

    :sparkles:	feat: models

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,43 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
+public class Endereco {
+   
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+    
+    public Endereco(DadosEndereco e) {
+        this.logradouro     = e.logradouro();
+        this.bairro         = e.bairro();
+        this.cep            = e.cep();
+        this.cidade         = e.cidade();
+        this.uf             = e.uf();
+        this.complemento    = e.complemento();
+        this.numero         = e.numero();
+    }
+
+    public void atualizarDados(DadosEndereco e) {
+        if (e.logradouro() != null)  this.logradouro    = e.logradouro();
+        if (e.bairro() != null)      this.bairro        = e.bairro();
+        if (e.cep() != null)         this.cep           = e.cep();
+        if (e.cidade() != null)      this.cidade        = e.cidade();
+        if (e.uf() != null)          this.uf            = e.uf();
+        if (e.complemento() != null) this.complemento   = e.complemento();
+        if (e.numero() != null)      this.numero        = e.numero();        
+    }
+}
