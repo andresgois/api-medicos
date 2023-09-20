1df06123593bb8fe6418770936a34cd2f26a8d6c
andre <andresousalee@gmail.com>
2023-09-20 19:20:22 -0300

    :speakles: feat: adicioando classe agendar

--- src/main/java/med/voll/api/repository/ConsultaRepository.java
+++ src/main/java/med/voll/api/repository/ConsultaRepository.java
@@ -3,5 +3,5 @@ package med.voll.api.repository;
 import med.voll.api.domain.consulta.Consuta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConsulatRepository extends JpaRepository<Consuta, Long> {
+public interface ConsultaRepository extends JpaRepository<Consuta, Long> {
 }
