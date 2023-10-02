0591c1aa05aa7484d83fd877f17d71e24722073c
andre <andresousalee@gmail.com>
2023-10-01 22:50:14 -0300

    :sparkles: feat: modificacao de consulta par cancelados

--- src/main/java/med/voll/api/repository/MedicoRepository.java
+++ src/main/java/med/voll/api/repository/MedicoRepository.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
     Page<Medico> findAllByAtivoTrue(Pageable pageable);
 
-    @Query("""
+    /*@Query("""
             select m from Medico m
             where
             m.ativo = true and
@@ -20,6 +20,25 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             order by rand()
             limit 1
             """)
+    Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);*/
+
+    @Query("""
+            select m from Medico m
+            where
+            m.ativo = 1
+            and
+            m.especialidade = :especialidade
+            and
+            m.id not in(
+                select c.medico.id from Consulta c
+                where
+                c.data = :data
+        and
+                c.motivoCancelamento is null
+            )
+            order by rand()
+            limit 1
+    """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
 
     @Query("""
